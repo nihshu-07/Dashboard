c2b3a0ddb8bc9cbbfd48b1d47d8992f880ca0a05
NIHARIKA TYAGI <niharika.tyagi710@gmail.com>
2025-09-22 20:31:15 +0530

    add data

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+
+st.set_page_config(page_title="Cars Dashboard", layout='wide')
+
+DATA_PATH = "car_cleaned.csv"  
