48f7dc086785c565f94f3ee31e3cdee18725b5d6
NIHARIKA TYAGI <niharika.tyagi710@gmail.com>
2025-09-23 12:58:33 +0530

    load data

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ import plotly.express as px
 st.set_page_config(page_title="Cars Dashboard", layout='wide')
 
 DATA_PATH = "car_cleaned.csv"  
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv(DATA_PATH)
+    return df
+
+df = load_data()
+st.write(df.head())
